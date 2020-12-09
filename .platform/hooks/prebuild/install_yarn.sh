794debc514183c8d987e27d32a104d69b16d9182
Ohad Fein <45171694+OhadFein@users.noreply.github.com>
2020-12-09 10:31:01 +0200

    Update install_yarn.sh

--- .platform/hooks/prebuild/install_yarn.sh
+++ .platform/hooks/prebuild/install_yarn.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
+sudo yum install -y nodejs
+
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
 sudo yum -y install yarn
