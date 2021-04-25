4e73d40203ed568ced6c8365f3fb5f12d9aa8789
LitalHamami <lital.hamami@gmail.com>
2021-04-25 21:28:46 +0300

    notifications-module

---
+++ packages/client/src/app/notifications/notifications-page/notifications-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dsapp-notifications-page',
+  templateUrl: './notifications-page.component.html',
+  styles: [
+  ]
+})
+export class NotificationsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
