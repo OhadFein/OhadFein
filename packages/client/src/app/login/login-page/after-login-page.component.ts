b4c63c450225dcb9594d6368643e99a47b185a3d
azimovMichael <azimov.m@gmail.com>
2021-07-03 15:00:37 +0300

    remove dash

--- packages/client/src/app/login/login-page/after-login-page.component.ts
+++ packages/client/src/app/login/login-page/after-login-page.component.ts
@@ -16,18 +16,24 @@ export class AfterLoginPageComponent implements OnInit {
   ) { }
 
   async ngOnInit() {
-    const userExists = await this.usersService.userExists()
-    if (!userExists) {
-      const loggedInUser = await Auth.currentUserInfo()
-      const username = this.extractUserName(loggedInUser)
-      await this.usersService.createNewUser(username, loggedInUser.attributes.sub)
+    try {
+      const userExists = await this.usersService.userExists()
+      if (!userExists) {
+        const loggedInUser = await Auth.currentUserInfo()
+        const username = this.extractUserName(loggedInUser)
+        await this.usersService.createNewUser(username, loggedInUser.attributes.sub)
+      }
+    }
+    catch (error) {
+      console.log("Failed after login logic")
+      await Auth.signOut()
     }
     this.router.navigate(['/student']);
   }
   extractUserName(loggedInUser) {
     const cognitoUsername: string= loggedInUser.username
     if (cognitoUsername.includes("facebook")) {
-      return loggedInUser.attributes.given_name.toLowerCase() + "-" + loggedInUser.attributes.family_name.toLowerCase()
+      return loggedInUser.attributes.given_name.toLowerCase() + loggedInUser.attributes.family_name.toLowerCase()
     }
     return cognitoUsername
   }
