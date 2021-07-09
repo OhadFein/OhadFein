6d0974c6a7d3f2ea819439d1cfda4873eb9ccc7d
Ohad Fein <ohad2121@gmail.com>
2021-07-09 14:39:25 +0300

    Fix ESLint

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'no-underscore-dangle': 'off',
+    'import/no-unresolved': 'off',
+    'no-useless-constructor': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
+};
