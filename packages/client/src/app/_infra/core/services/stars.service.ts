8059e00f777d11a0b2b8e8ca322ea57e87b36bd6
Andrey Sverkunov <andrey.sverkunov@comtravo.com>
2021-07-31 10:01:52 +0200

    adjust comments

--- packages/client/src/app/_infra/core/services/stars.service.ts
+++ packages/client/src/app/_infra/core/services/stars.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 import { StarDto } from '@danskill/contract';
-import { IStarContent, IRestResponse } from '@core/models';
 
 import { BaseRestService } from './base-rest.service';
 
@@ -12,12 +10,6 @@ import { BaseRestService } from './base-rest.service';
 })
 export class StarsService {
   constructor(private baseRestService: BaseRestService) {}
-
-  getStarContent(starId: string): Observable<IStarContent> {
-    return this.baseRestService
-      .get<IRestResponse>(`stars/${starId}`)
-      .pipe(map((response) => response.data ?? []));
-  }
 
   getStars(): Observable<StarDto[]> {
     return this.baseRestService.get<StarDto[]>('users/all/stars');
--- packages/client/src/app/_infra/store/effects/index.ts
+++ packages/client/src/app/_infra/store/effects/index.ts
@@ -1,6 +1,5 @@
 export * from './stars.effects';
 export * from './user.effects';
-export * from './stars-content.effects';
 export * from './practices.effects';
 export * from './figures.effects';
 export * from './notifications.effects';
