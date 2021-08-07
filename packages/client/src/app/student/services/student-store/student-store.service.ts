6dd4de3e81e88649b70fce83bf3e17e3c590beb8
Andrey Sverkunov <andrey.sverkunov@comtravo.com>
2021-08-07 22:51:13 +0200

    add store for student route

---
+++ packages/client/src/app/student/services/student-store/student-store.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { StarDto } from '@danskill/contract';
+
+export interface StudentStore {
+  stars: StarDto[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentStoreService {
+  private store: StudentStore = {
+    stars: []
+  };
+
+  setStars(payload: StarDto[]): void {
+    this.store.stars = [...payload];
+  }
+
+  getAllStars(): StarDto[] {
+    return this.store.stars;
+  }
+
+  getStarBySlug(slug: string): StarDto | null {
+    return this.store.stars.find((star: StarDto) => star.slug === slug);
+  }
+}
