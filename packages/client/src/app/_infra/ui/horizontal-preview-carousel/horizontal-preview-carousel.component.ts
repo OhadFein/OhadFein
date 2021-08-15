17a3453b402bd62810f72b7cb527b61865b8a6e5
Andrey Sverkunov <andrey.sverkunov@comtravo.com>
2021-08-15 15:50:24 +0200

    add horizontal-preview-carousel component

---
+++ packages/client/src/app/_infra/ui/horizontal-preview-carousel/horizontal-preview-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { FigureBaseDto } from '@danskill/contract';
+
+@Component({
+  selector: 'dsapp-horizontal-preview-carousel',
+  templateUrl: './horizontal-preview-carousel.component.html',
+  styleUrls: ['./horizontal-preview-carousel.component.scss']
+})
+export class HorizontalPreviewCarouselComponent {
+  @Input()
+  name: string;
+
+  @Input()
+  items: FigureBaseDto[];
+}
