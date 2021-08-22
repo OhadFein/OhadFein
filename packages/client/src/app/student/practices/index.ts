c50508d2c5d26ba11dc08fdfed85cc580dd96d22
azimovMichael <azimov.m@gmail.com>
2021-08-22 22:57:29 +0300

    extracted note template

--- packages/client/src/app/student/practices/index.ts
+++ packages/client/src/app/student/practices/index.ts
@@ -1,4 +1,4 @@
 export * from './practices-page/practices-page.component';
 export * from './practice-page/practice-page.component';
-export * from './notes/new-note-page.component';
+export * from './notes/new-note-page/new-note-page.component';
 export * from './notes/note-template/note.component';
