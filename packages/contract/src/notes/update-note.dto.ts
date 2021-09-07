03ad92ee891f36912f66942d43c538689d82bd89
Ohad Fein <ohad2121@gmail.com>
2021-09-07 14:40:35 +0300

    Added ctor to UpdateNoteDto

--- packages/contract/src/notes/update-note.dto.ts
+++ packages/contract/src/notes/update-note.dto.ts
@@ -1,6 +1,11 @@
 import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateNoteDto {
+  constructor(title: string, content: string) {
+    this.title = title;
+    this.content = content;
+  }
+
   @IsString()
   title: string;
 
